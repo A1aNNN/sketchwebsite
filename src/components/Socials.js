650d1a86ed836690440308836f86728d3d54e681
Alan Wang <alanwang123919@gmail.com>
2023-09-01 00:39:32 -0700

    added icons in

---
+++ src/components/Socials.js
@@ -0,0 +1,28 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import linkedin from './../images/LinkedIn.png'
+import youtube from './../images/YouTube.png'
+import github from './../images/Github.png'
+
+const Socials = () => {
+  return (
+    <Box className='componentCard'>
+        <h1>Socials</h1>
+        <h2 className='socialsDescription'>If you want to connect with me on other platforms!</h2>
+
+        <Box display="flex" flexDirection="row" paddingTop='40px'>
+            <a href='https://www.linkedin.com/in/alan-wang-a577b81b5/' target='_blank'>
+                <img className='socials linkedin' src={linkedin}/>
+            </a>
+            <a href='https://www.youtube.com/@alanwang093/featured' target='_blank'>
+                <img className='socials youtube' src={youtube}/>
+            </a>
+            <a href='https://github.com/A1aNNN' target='_blank'>
+                <img className='socials github' src={github}/>
+            </a>
+        </Box>
+    </Box>
+  )
+}
+
+export default Socials
