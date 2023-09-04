d4cb858b5ad06adb1e749238e838126906db17bc
Alan Wang <alanwang123919@gmail.com>
2023-09-03 23:04:23 -0700

    .

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import WordsTypewriter from './WordsTypewriter'
+import headshot from './../images/headshot.jpg'
 
 const AboutMe = () => {
   return (
@@ -8,6 +9,11 @@ const AboutMe = () => {
       <h1>
         About me
       </h1>
+      
+
+      <Box className='headshotContainer'>
+        <img className='headshotPhoto' src={headshot}/>
+      </Box>
 
       <h2 className='introduction'>
         Hey! My name's Alan!
@@ -15,9 +21,6 @@ const AboutMe = () => {
 
       <WordsTypewriter/>
 
-      {/* <h2 className='aboutText'>
-        Just an undergraduate student trekking through life.
-      </h2> */}
       <h2 className='aboutText'>
         Just an undergraduate student trekking through life. 
         When I'm not in class or working, you might catch me at the gym getting a workout in, or chilling at home struggling to make a 3D donut on Blender.
