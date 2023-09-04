ba050439f242b5142bf35c79caf9ad0a748c3116
Alan Wang <alanwang123919@gmail.com>
2023-09-03 23:03:47 -0700

    added Youtube video

---
+++ src/components/Youtube.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react';
+
+
+const Youtube = () => {
+  return (
+    <Box className='youtube'>
+        <div className="ratio ratio-16x9 youtubeVideo">
+            <iframe src="https://www.youtube.com/embed/0N3PAzlHetk" title="My Handstand Pushup Progression" allowfullscreen
+                width="580" height="327" />
+        </div>
+        <Box className="youtubeText">
+            I've been trying to practice my handstands this past year.
+            This is how I attempted to learn the handstand pushup.
+        </Box>
+    </Box>
+  )
+}
+
+export default Youtube
